c9d8b807ee094b2528a603021a23d4d979e8e1d5
MD-Mujtaba <78491215+MD-Mujtaba@users.noreply.github.com>
2021-02-03 20:41:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var diceImage1 = "dice" + randomNumber1 + ".png";
+
+var imageSource1 = "images/" +diceImage1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",imageSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var diceImage2 = "dice" + randomNumber2+ ".png";
+
+var imageSource2 = "images/" +diceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src",imageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins!";
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML="player 2 wins!";
+}else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
